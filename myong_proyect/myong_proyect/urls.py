4ac68ab93f7d322242831a075640a00a85da6c98
rhernandezmy <rhernandezmy@fpvirtualaragon.es>
2025-12-30 22:29:33 +0100

    Modificacion y creacion archivo urls en socios

--- myong_proyect/myong_proyect/urls.py
+++ myong_proyect/myong_proyect/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+from myong_proyect import socios
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Añadimos la url de la aplicacion socios
+    path('socios/', include(socios.urls)),
 ]
