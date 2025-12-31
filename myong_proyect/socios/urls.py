503a554d10e36b1dd253c5675489536c5bff6856
rhernandezmy <rhernandezmy@fpvirtualaragon.es>
2025-12-31 16:34:35 +0100

    modificado socio y añadido tutor

--- myong_proyect/socios/urls.py
+++ myong_proyect/socios/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from . import views
-
+from socios.views import detalle_socio, lista_socios, alta_socio, pagos_socio_current_year, pagos_socio_by_year
 urlpatterns = [
-    path('', views.index, name='index'),  # Página principal de socios
+    path('<uuid:socio_id>/', detalle_socio, name='detalle_socio'),
+    path('', lista_socios, name='lista_socios'),
+    # path('alta/', alta_socio, name='alta_socio'),
+    # path('<uuid:socio_id>/pagos/<int:year>', pagos_socio_by_year, name='pagos_socio_by_year'),
+    # path('<uuid:socio_id>/pagos/', pagos_socio_current_year, name='pagos_socio_current_year'), 
 ]
