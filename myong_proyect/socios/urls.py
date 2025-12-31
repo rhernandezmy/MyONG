fbc6610e9a6ee29a9c96d0f5b7a93610d61cc97b
rhernandezmy <rhernandezmy@fpvirtualaragon.es>
2025-12-31 20:18:33 +0100

    Sigue fallando la BBDD

--- myong_proyect/socios/urls.py
+++ myong_proyect/socios/urls.py
@@ -3,7 +3,7 @@ from socios.views import detalle_socio, lista_socios, alta_socio, pagos_socio_cu
 urlpatterns = [
     path('<uuid:socio_id>/', detalle_socio, name='detalle_socio'),
     path('', lista_socios, name='lista_socios'),
-    # path('alta/', alta_socio, name='alta_socio'),
-    # path('<uuid:socio_id>/pagos/<int:year>', pagos_socio_by_year, name='pagos_socio_by_year'),
-    # path('<uuid:socio_id>/pagos/', pagos_socio_current_year, name='pagos_socio_current_year'), 
+    path('alta/', alta_socio, name='alta_socio'),
+    path('<uuid:socio_id>/pagos/<int:year>', pagos_socio_by_year, name='pagos_socio_by_year'),
+    path('<uuid:socio_id>/pagos/', pagos_socio_current_year, name='pagos_socio_current_year'), 
 ]
