4be4e750f42045b697e01e360aca44b40ac97778
rhernandezmy <rhernandezmy@fpvirtualaragon.es>
2025-12-31 18:57:20 +0100

    Creacion archivo .env

--- myong_proyect/myong_proyect/settings.py
+++ myong_proyect/myong_proyect/settings.py
@@ -41,7 +41,7 @@ ROOT_URLCONF = 'myong_proyect.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
