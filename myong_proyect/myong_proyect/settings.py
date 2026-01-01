095479f27029a116a5eedae3caa45313a8c6e53f
rhernandezmy <rhernandezmy@fpvirtualaragon.es>
2026-01-01 20:44:00 +0100

    cambio en setting localhost por 127.0.0.1

--- myong_proyect/myong_proyect/settings.py
+++ myong_proyect/myong_proyect/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST', default='localhost'),
+        'HOST': config('DB_HOST', default='127.0.0.1'),
         'PORT': config('DB_PORT', default='5432', cast=int),
     }
 }
